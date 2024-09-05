1a436f275dc2a9b57ac647598e6f6086b2518d4a
Exitedelectron <115715828+Exitedelectron@users.noreply.github.com>
2024-09-05 13:05:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import os
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 import webserver
 
 TOKEN = os.getenv('TOKEN')
 # Configura gli intenti per il bot
 intents = discord.Intents.default()
-intents.message_content = True  # Abilita l'accesso al contenuto dei messaggi
+intents.message_content = True
 
-# Se hai bisogno di intenti per membri o presenze, usa anche questi
-# intents.members = True
-# intents.presences = True
-
-# Crea l'istanza del bot con il prefisso dei comandi e gli intenti specificati
+# Crea l'istanza del bot
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Crea l'istanza per i comandi slash
+slash = SlashCommand(bot, sync_commands=True)
 
 # Evento che viene chiamato quando il bot si connette e si è connesso a Discord
 @bot.event
 async def on_ready():
     print(f'{bot.user} è connesso a Discord!')
 
-# Comando semplice che risponde con "Ciao!" quando l'utente invia il comando !ciao
-@bot.command(name='ciao')
-async def ciao(ctx):
+# Comando slash che risponde con "Ciao!" quando l'utente invia il comando /ciao
+@slash.slash(name='ciao', description='Risponde con Ciao!')
+async def ciao(ctx: SlashContext):
     await ctx.send('Ciao!')
 
 # Avvio del bot (inserisci il tuo token del bot qui)
